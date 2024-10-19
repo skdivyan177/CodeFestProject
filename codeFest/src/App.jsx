189ee683ff18e75fbb1640c5fc7ca7bc1519357f
Sy Traore <132951255+sytraore@users.noreply.github.com>
2024-10-19 13:58:15 -0400

    added opacity

--- codeFest/src/App.jsx
+++ codeFest/src/App.jsx
@@ -7,11 +7,11 @@ const App = () => {
 
   return (
   <div className="image-container">
-    <img
+    {/* <img
         src="https://brandslogos.com/wp-content/uploads/images/large/marriott-logo.png"
         alt="alt"
         className="small-image" 
-      />
+      /> */}
       
     <img
       src="https://cache.marriott.com/is/image/marriotts7prod/fllsb-view-0077:Pano-Hor?wid=1600&fit=constrain"
