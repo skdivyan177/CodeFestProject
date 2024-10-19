c1c877d4759469f6765ff4ea62e5a73c78a9208a
Divyan Dhavale <137909253+skdivyan177@users.noreply.github.com>
2024-10-19 09:34:53 -0400

    Update HMP.java

--- HMP.java
+++ HMP.java
@@ -1,18 +1,18 @@
 import java.util.HashMap;
-
 public class HMP {
     public static void main(String[] args)
     {
+        // making a hash map with 2 strings here 
         HashMap<String, String> map = new HashMap<>();
         map.put("", "");
         map.put("", "");
         map.put("", "");
-        
         System.out.println(map);
-        if (map.containsKey("")) {
-            Integer a = map.get("");
+        // if the map contains the key, get it and print it
+        if (map.containsKey("Key1")) {
+            String a = map.get("Key1");
             System.out.println("value for key"
-                               + " \"\" is: " + a);
+                               + " \"Key1\" is: " + a);
         }
     }
 }
