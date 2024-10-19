77624da0c44a60d874d93a2c70417be826e22713
ddivyan117 <137909253+skdivyan177@users.noreply.github.com>
2024-10-19 13:23:26 -0400

    second lauyout trial

--- codeFest/src/App.jsx
+++ codeFest/src/App.jsx
@@ -7,6 +7,8 @@ const App = () => {
 
   return (
   <div className="image-container">
+    <h1> Destination Calculator </h1>
+    
     <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4dxlhJfmDGIMT_m1uv3wXBnoNpOt8SzE9paipVBpcfAz8CIymcnTViGbOt2GVYRn_Sng&usqp=CAU"
         alt="alt"
@@ -17,7 +19,23 @@ const App = () => {
       alt="alt"
       className="responsive-image"
     />
-    
+    <button type="button">Home</button>
+    <button type="button">About Us</button>
+    <button type="button">Contact</button>
+    <button type="button">Language 🌐</button>
+
+    {/* <button type="button" onClick={handleClick}>
+      Home
+    </button>
+    <button type="button" onClick={handleClick}>
+      About Us 
+    </button>
+    <button type="button" onClick={handleClick}>
+      Contact
+    </button>
+    <button type="button" onClick={handleClick}>
+      Language 🌐
+    </button> */}
   </div>  
   )
 }
