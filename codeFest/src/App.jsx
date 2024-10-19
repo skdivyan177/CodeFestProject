e1c2165885f7642577112d205b33ec675853b599
ddivyan117 <137909253+skdivyan177@users.noreply.github.com>
2024-10-19 17:04:54 -0400

    sixth layout trial

--- codeFest/src/App.jsx
+++ codeFest/src/App.jsx
@@ -7,8 +7,9 @@ const App = () => {
 
   return (
   <div className="image-container">
-    <h1> Mariott </h1>
-    <h1> Destination Calculator </h1>
+
+  <h1 class="main-title">Marriott</h1>
+  <h1 class="sub-title">Destination Calculator</h1>
 
     <img
         src="https://i0.wp.com/www.dafontfree.io/wp-content/uploads/2023/11/marriott-logo.png?resize=1536%2C1007&ssl=1"
