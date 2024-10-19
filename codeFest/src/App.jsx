ddeffdc2a8a24b75b4e216bc876858b8dfa0b82d
sabir196 <sabir23@vt.edu>
2024-10-19 14:38:53 -0400

    sh

--- codeFest/src/App.jsx
+++ codeFest/src/App.jsx
@@ -8,11 +8,11 @@ const App = () => {
   return (
   <div className="image-container">
     <img
-        src="https://brandslogos.com/wp-content/uploads/images/large/marriott-logo.png"
+        src="https://i0.wp.com/www.dafontfree.io/wp-content/uploads/2023/11/marriott-logo.png?resize=1536%2C1007&ssl=1"
         alt="alt"
         className="small-image" 
       />
-      
+
     <img
       src="https://cache.marriott.com/is/image/marriotts7prod/fllsb-view-0077:Pano-Hor?wid=1600&fit=constrain"
       alt="alt"
